1c444f893105306912f45afbb9766e5171a62204
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-10-10 14:55:43 -0300

    DBService and testConfig

--- digitalPoint/src/main/java/com/sevensolutions/digitalpoint/DigitalPointApplication.java
+++ digitalPoint/src/main/java/com/sevensolutions/digitalpoint/DigitalPointApplication.java
@@ -18,41 +18,10 @@ import java.time.LocalTime;
 import java.util.Date;
 
 @SpringBootApplication
-public class DigitalPointApplication implements CommandLineRunner {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PointRepository pointRepository;
+public class DigitalPointApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DigitalPointApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception{
-
-        User user1 = new User(null, "Bianca Santos","3427");
-        user1.addProfile(Profile.ADMIN);
-        User user2 = new User(null, "Gabriel Santana","4536");
-        User user3 = new User(null, "Lucas Simões","2745");
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Date workDay = dateFormat.parse("01/10/2024");
-
-        Point point1 = new Point(null, user1.getName(), workDay, LocalTime.parse("08:00"), LocalTime.parse("10:00"), LocalTime.parse("12:00"), LocalTime.parse("15:00"), user1);
-        Point point2 = new Point(null, user2.getName(), workDay, LocalTime.parse("08:00"), LocalTime.parse("10:00"), LocalTime.parse("12:00"), LocalTime.parse("15:00"), user2);
-        Point point3 = new Point(null, user3.getName(), workDay, LocalTime.parse("08:00"), LocalTime.parse("10:00"), LocalTime.parse("12:00"), LocalTime.parse("15:00"), user3);
-
-        userRepository.saveAll(Arrays.asList(user1));
-        userRepository.saveAll(Arrays.asList(user2));
-        userRepository.saveAll(Arrays.asList(user3));
-
-        pointRepository.saveAll(Arrays.asList(point1));
-        pointRepository.saveAll(Arrays.asList(point2));
-        pointRepository.saveAll(Arrays.asList(point3));
-
     }
 
     @Bean

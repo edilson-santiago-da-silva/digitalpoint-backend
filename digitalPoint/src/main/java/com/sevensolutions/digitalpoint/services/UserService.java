0339461c1fdaf4f9358c8fd16635ebe063056c83
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-10-18 16:27:34 -0300

    exception for object user not found

--- digitalPoint/src/main/java/com/sevensolutions/digitalpoint/services/UserService.java
+++ digitalPoint/src/main/java/com/sevensolutions/digitalpoint/services/UserService.java
@@ -2,6 +2,7 @@ package com.sevensolutions.digitalpoint.services;
 
 import com.sevensolutions.digitalpoint.domain.User;
 import com.sevensolutions.digitalpoint.repositores.UserRepository;
+import com.sevensolutions.digitalpoint.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class UserService {
 
     public User findById(Integer id){
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectnotFoundException("object not found! id " + id ));
     }
 }

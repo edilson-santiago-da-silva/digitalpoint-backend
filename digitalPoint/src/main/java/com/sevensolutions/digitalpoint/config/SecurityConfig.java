923837dd95615267ce3ba1cb4da9f44f9a29925c
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-12-04 09:20:56 -0300

    feat: JWT class SecurityConfig and Environment interface

--- digitalPoint/src/main/java/com/sevensolutions/digitalpoint/config/SecurityConfig.java
+++ digitalPoint/src/main/java/com/sevensolutions/digitalpoint/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.sevensolutions.digitalpoint.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -11,12 +14,30 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
+
+    private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
+
+    @Autowired
+    private Environment env;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+            }
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_MATCHERS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+
     }
 
     @Bean
